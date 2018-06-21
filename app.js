9173a868355693124ac933f6f6a34f5093f6bc8a
tripott <tripott@gmail.com>
2018-06-21 14:35:12 -0400

    checking request body for errors

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 require('dotenv').config()
 const app = require('express')()
 const port = process.env.PORT || 5001
-
 const bodyParser = require('body-parser')
 const NodeHTTPError = require('node-http-error')
-
+const { propOr, isEmpty, compose, not } = require('ramda')
+const reqFieldChecker = require('./lib/required-field-check')
 app.use(bodyParser.json())
 
 app.get('/', function(req, res, next) {
   res.send('Welcome to the brocean, brah.  Gnar Gnar.')
+})
+
+app.post('/boards', (req, res, next) => {
+  const newBoard = propOr({}, 'body', req)
+
+  if (isEmpty(newBoard)) {
+    next(
+      new NodeHTTPError(
+        400,
+        'Brah, add a board to the request body.  Ensure the Content-Type is application/json. Dude!'
+      )
+    )
+  }
+
+  const sendMissingFieldError = compose(
+    not,
+    isEmpty,
+    reqFieldChecker(['name', 'category', 'price', 'sku'])
+  )(newBoard)
+
+  if (sendMissingFieldError) {
+    next(
+      new NodeHTTPError(400, 'Brah, you didnt pass all the required fields.')
+    )
+  }
 })
 
 app.use((err, req, res, next) => {
