9d09ecdceaef90342ff679f31b8ccd599d1eac85
Will Adkins <wkadki01@gmail.com>
2018-06-22 15:40:12 -0400

    Up to single filterQuery, limit, and pagination options for list

--- app.js
+++ app.js
@@ -3,14 +3,96 @@ const app = require('express')()
 const port = process.env.PORT || 5001
 const bodyParser = require('body-parser')
 const NodeHTTPError = require('node-http-error')
-const { propOr, isEmpty, compose, not, join } = require('ramda')
+const {
+  propOr,
+  isEmpty,
+  compose,
+  not,
+  join,
+  pick,
+  pathOr,
+  split
+} = require('ramda')
 const reqFieldChecker = require('./lib/required-field-check')
-const { addBoard } = require('./dal')
+const {
+  addBoard,
+  replaceBoard,
+  getBoard,
+  deleteBoard,
+  listBoards,
+  filterListBoards
+} = require('./dal')
 
 app.use(bodyParser.json())
 
 app.get('/', function(req, res, next) {
   res.send('Welcome to the brocean, brah.  Gnar Gnar.')
+})
+
+app.get('/boards', function(req, res, next) {
+  const limit = Number(pathOr(100, ['query', 'limit'], req))
+  const paginate = pathOr(null, ['query', 'start_key'], req)
+  const filterQuery = pathOr('type:board', ['query', 'filter'], req)
+
+  // listBoards(limit, paginate)
+  //   .then(list => res.status(200).send(list))
+  //   .catch(err => next(new NodeHTTPError(err.status, err.message, err)))
+
+  filterListBoards(filterQuery, limit, paginate)
+    .then(list => res.status(200).send(list))
+    .catch(err => next(new NodeHTTPError(err.status, err.message, err)))
+})
+
+app.get('/boards/:sku', function(req, res, next) {
+  const sku = pathOr('', ['params', 'sku'], req)
+
+  getBoard(sku)
+    .then(board => res.status(200).send(board))
+    .catch(err => next(new NodeHTTPError(err.status, err.message, err)))
+})
+
+app.put('/boards/:sku', function(req, res, next) {
+  const newBoard = propOr({}, 'body', req)
+
+  if (isEmpty(newBoard)) {
+    next(
+      new NodeHTTPError(
+        400,
+        'Brah, add a board to the request body.  Ensure the Content-Type is application/json. Dude!'
+      )
+    )
+  }
+
+  const missingFields = reqFieldChecker(
+    ['type', '_id', '_rev', 'name', 'category', 'price', 'sku'],
+    newBoard
+  )
+  const missingFieldError = compose(
+    not,
+    isEmpty
+  )(missingFields)
+
+  if (missingFieldError) {
+    next(
+      new NodeHTTPError(
+        400,
+        `Brah, you didnt pass all the required fields: ${join(
+          ', ',
+          missingFields
+        )}`,
+        { josh: 'is Cool and humble', jp: 'is Cool' }
+      )
+    )
+  }
+
+  const cleanBoard = pick(
+    ['type', '_id', '_rev', 'name', 'category', 'price', 'sku'],
+    newBoard
+  )
+
+  replaceBoard(cleanBoard)
+    .then(replaceResult => res.status(200).send(replaceResult))
+    .catch(err => next(new NodeHTTPError(err.status, err.message, err)))
 })
 
 app.post('/boards', (req, res, next) => {
@@ -30,7 +112,10 @@ app.post('/boards', (req, res, next) => {
     newBoard
   )
 
-  const sendMissingFieldError = compose(not, isEmpty)(missingFields)
+  const sendMissingFieldError = compose(
+    not,
+    isEmpty
+  )(missingFields)
 
   if (sendMissingFieldError) {
     next(
@@ -39,19 +124,28 @@ app.post('/boards', (req, res, next) => {
         `Brah, you didnt pass all the required fields: ${join(
           ', ',
           missingFields
-        )}`,
-        { josh: 'is Cool and humble', jp: 'is Cool' }
+        )}`
       )
     )
   }
 
-  addBoard(newBoard, function(err, result) {
-    if (err)
+  const cleanBoard = pick(['name', 'category', 'price', 'sku'], newBoard)
+
+  addBoard(cleanBoard)
+    .then(result => res.status(201).send(result))
+    .catch(err =>
       next(
         new NodeHTTPError(err.status, err.message, { ...err, max: 'isCool' })
       )
-    res.status(201).send(result)
-  })
+    )
+})
+
+app.delete('/boards/:sku', function(req, res, next) {
+  const sku = pathOr('', ['params', 'sku'], req)
+
+  deleteBoard(sku)
+    .then(deleteResult => res.status(200).send(deleteResult))
+    .catch(err => next(new NodeHTTPError(err.status, err.message, err)))
 })
 
 app.use((err, req, res, next) => {
--- dal.js
+++ dal.js
@@ -1,20 +1,62 @@
 require('dotenv').config()
 const PouchDB = require('pouchdb-core')
 PouchDB.plugin(require('pouchdb-adapter-http'))
+PouchDB.plugin(require('pouchdb-find'))
 
-const { merge, prop } = require('ramda')
+const { merge, map, propOr, prop, split } = require('ramda')
 const pkGen = require('./lib/pk-gen')
 
 const db = new PouchDB(
   `${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
 )
 
-const addBoard = (board, cb) => {
+const addBoard = board => {
   const modifiedBoard = merge(board, {
     type: 'board',
     _id: pkGen('board_', '-', prop('sku', board))
   })
-  db.put(modifiedBoard, cb)
+  return db.put(modifiedBoard)
 }
 
-module.exports = { addBoard }
+const replaceBoard = newBoard => db.put(newBoard)
+
+const getBoard = sku => db.get(`board_${sku}`)
+
+const deleteBoard = sku =>
+  db.get(`board_${sku}`).then(result => db.remove(result))
+
+const listBoards = (limit, paginate) => {
+  return db
+    .allDocs(
+      paginate
+        ? { include_docs: true, limit: limit, start_key: `${paginate}\ufff0` }
+        : { include_docs: true, limit: limit, start_key: 'board_' }
+    )
+    .then(boards => map(prop('doc'), propOr([], 'rows', boards)))
+}
+
+const filterListBoards = (filterQueryStr, limitStr, paginate) => {
+  const [filterKey, filterValue] = split(':', filterQueryStr)
+
+  // const selector = paginate
+  //   ? { _id: { $gt: `${paginate}\ufff0` }, [`${filterKey}`]: filterValue }
+  //   : { _id: { $regex: 'board_' }, [`${filterKey}`]: filterValue }
+
+  return db
+    .find({
+      selector: paginate
+        ? { _id: { $gt: `${paginate}\ufff0` }, [`${filterKey}`]: filterValue }
+        : { _id: { $regex: 'board_' }, [`${filterKey}`]: filterValue },
+      limit: Number(limitStr)
+    })
+    .then(bookmark => bookmark.docs)
+}
+
+module.exports = {
+  addBoard,
+  replaceBoard,
+  getBoard,
+  deleteBoard,
+  listBoards,
+  filterListBoards
+}
